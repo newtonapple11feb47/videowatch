3d9e4acc9916b566a7096eaa2156d39f720e0238
noone <nothing@example.com>
2019-09-17 13:16:56 +0000

    done

---
+++ cc-core/system/config.php
@@ -0,0 +1,16 @@
+<?php
+
+// Database Credentials
+define ('DB_HOST', 'localhost');
+define ('DB_PORT', '3306');
+define ('DB_USER', 'root');
+define ('DB_PASS', 'root');
+define ('DB_NAME', 'edifyn');
+define ('DB_PREFIX', 'video');
+
+// FTP Credentials
+define ('FTP_HOST', '');
+define ('FTP_USER', '');
+define ('FTP_PASS', '');
+define ('FTP_PATH', '');
+define ('FTP_SSL', false);
